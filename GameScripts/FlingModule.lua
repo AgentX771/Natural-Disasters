929b846e414d28ea0be4e443b1087be440188527
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-02-11 22:59:36 -0600

    Update FlingModule.lua

--- GameScripts/FlingModule.lua
+++ GameScripts/FlingModule.lua
@@ -13,27 +13,21 @@ local GetPlayer = function(Name)
         return
     elseif Name == "random" then
         local GetPlayers = Players:GetPlayers()
-        if table.find(GetPlayers, Player) then
-            table.remove(GetPlayers, table.find(GetPlayers, Player))
-        end
+        if table.find(GetPlayers,Player) then table.remove(GetPlayers,table.find(GetPlayers,Player)) end
         return GetPlayers[math.random(#GetPlayers)]
     elseif Name ~= "random" and Name ~= "all" and Name ~= "others" then
-        for _, x in next, Players:GetPlayers() do
+        for _,x in next, Players:GetPlayers() do
             if x ~= Player then
-                if x.Name:lower():match("^" .. Name) then
-                    return x
-                elseif x.DisplayName:lower():match("^" .. Name) then
-                    return x
+                if x.Name:lower():match("^"..Name) then
+                    return x;
+                elseif x.DisplayName:lower():match("^"..Name) then
+                    return x;
                 end
             end
         end
     else
         return
     end
-end
-
-local Message = function(_Title, _Text, Time)
-    game:GetService("StarterGui"):SetCore("SendNotification", {Title = _Title, Text = _Text, Duration = Time})
 end
 
 local SkidFling = function(TargetPlayer)
@@ -78,14 +72,14 @@ local SkidFling = function(TargetPlayer)
         if not TCharacter:FindFirstChildWhichIsA("BasePart") then
             return
         end
-
+        
         local FPos = function(BasePart, Pos, Ang)
             RootPart.CFrame = CFrame.new(BasePart.Position) * Pos * Ang
             Character:SetPrimaryPartCFrame(CFrame.new(BasePart.Position) * Pos * Ang)
             RootPart.Velocity = Vector3.new(9e7, 9e7 * 10, 9e7)
             RootPart.RotVelocity = Vector3.new(9e8, 9e8, 9e8)
         end
-
+        
         local SFBasePart = function(BasePart)
             local TimeToWait = 2
             local Time = tick()
@@ -96,46 +90,22 @@ local SkidFling = function(TargetPlayer)
                     if BasePart.Velocity.Magnitude < 50 then
                         Angle = Angle + 100
 
-                        FPos(
-                            BasePart,
-                            CFrame.new(0, 1.5, 0) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25,
-                            CFrame.Angles(math.rad(Angle), 0, 0)
-                        )
+                        FPos(BasePart, CFrame.new(0, 1.5, 0) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle),0 ,0))
                         task.wait()
 
-                        FPos(
-                            BasePart,
-                            CFrame.new(0, -1.5, 0) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25,
-                            CFrame.Angles(math.rad(Angle), 0, 0)
-                        )
+                        FPos(BasePart, CFrame.new(0, -1.5, 0) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
 
-                        FPos(
-                            BasePart,
-                            CFrame.new(2.25, 1.5, -2.25) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25,
-                            CFrame.Angles(math.rad(Angle), 0, 0)
-                        )
+                        FPos(BasePart, CFrame.new(2.25, 1.5, -2.25) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
 
-                        FPos(
-                            BasePart,
-                            CFrame.new(-2.25, -1.5, 2.25) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25,
-                            CFrame.Angles(math.rad(Angle), 0, 0)
-                        )
+                        FPos(BasePart, CFrame.new(-2.25, -1.5, 2.25) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
 
-                        FPos(
-                            BasePart,
-                            CFrame.new(0, 1.5, 0) + THumanoid.MoveDirection,
-                            CFrame.Angles(math.rad(Angle), 0, 0)
-                        )
+                        FPos(BasePart, CFrame.new(0, 1.5, 0) + THumanoid.MoveDirection,CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
 
-                        FPos(
-                            BasePart,
-                            CFrame.new(0, -1.5, 0) + THumanoid.MoveDirection,
-                            CFrame.Angles(math.rad(Angle), 0, 0)
-                        )
+                        FPos(BasePart, CFrame.new(0, -1.5, 0) + THumanoid.MoveDirection,CFrame.Angles(math.rad(Angle), 0, 0))
                         task.wait()
                     else
                         FPos(BasePart, CFrame.new(0, 1.5, THumanoid.WalkSpeed), CFrame.Angles(math.rad(90), 0, 0))
@@ -146,26 +116,14 @@ local SkidFling = function(TargetPlayer)
 
                         FPos(BasePart, CFrame.new(0, 1.5, THumanoid.WalkSpeed), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
-
-                        FPos(
-                            BasePart,
-                            CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25),
-                            CFrame.Angles(math.rad(90), 0, 0)
-                        )
+                        
+                        FPos(BasePart, CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
 
-                        FPos(
-                            BasePart,
-                            CFrame.new(0, -1.5, -TRootPart.Velocity.Magnitude / 1.25),
-                            CFrame.Angles(0, 0, 0)
-                        )
+                        FPos(BasePart, CFrame.new(0, -1.5, -TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(0, 0, 0))
                         task.wait()
 
-                        FPos(
-                            BasePart,
-                            CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25),
-                            CFrame.Angles(math.rad(90), 0, 0)
-                        )
+                        FPos(BasePart, CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
 
                         FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(math.rad(90), 0, 0))
@@ -174,7 +132,7 @@ local SkidFling = function(TargetPlayer)
                         FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(0, 0, 0))
                         task.wait()
 
-                        FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(math.rad(-90), 0, 0))
+                        FPos(BasePart, CFrame.new(0, -1.5 ,0), CFrame.Angles(math.rad(-90), 0, 0))
                         task.wait()
 
                         FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(0, 0, 0))
@@ -183,24 +141,19 @@ local SkidFling = function(TargetPlayer)
                 else
                     break
                 end
-            until BasePart.Velocity.Magnitude > 500 or BasePart.Parent ~= TargetPlayer.Character or
-                TargetPlayer.Parent ~= Players or
-                not TargetPlayer.Character == TCharacter or
-                THumanoid.Sit or
-                Humanoid.Health <= 0 or
-                tick() > Time + TimeToWait
+            until BasePart.Velocity.Magnitude > 500 or BasePart.Parent ~= TargetPlayer.Character or TargetPlayer.Parent ~= Players or not TargetPlayer.Character == TCharacter or THumanoid.Sit or Humanoid.Health <= 0 or tick() > Time + TimeToWait
         end
-
-        workspace.FallenPartsDestroyHeight = 0 / 0
-
+        
+        workspace.FallenPartsDestroyHeight = 0/0
+        
         local BV = Instance.new("BodyVelocity")
         BV.Name = "EpixVel"
         BV.Parent = RootPart
         BV.Velocity = Vector3.new(9e8, 9e8, 9e8)
-        BV.MaxForce = Vector3.new(1 / 0, 1 / 0, 1 / 0)
-
+        BV.MaxForce = Vector3.new(1/0, 1/0, 1/0)
+        
         Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, false)
-
+        
         if TRootPart and THead then
             if (TRootPart.CFrame.p - THead.CFrame.p).Magnitude > 5 then
                 SFBasePart(THead)
@@ -214,47 +167,38 @@ local SkidFling = function(TargetPlayer)
         elseif not TRootPart and not THead and Accessory and Handle then
             SFBasePart(Handle)
         end
-
+        
         BV:Destroy()
         Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, true)
         workspace.CurrentCamera.CameraSubject = Humanoid
-
+        
         repeat
             RootPart.CFrame = getgenv().OldPos * CFrame.new(0, .5, 0)
             Character:SetPrimaryPartCFrame(getgenv().OldPos * CFrame.new(0, .5, 0))
             Humanoid:ChangeState("GettingUp")
-            table.foreach(
-                Character:GetChildren(),
-                function(_, x)
-                    if x:IsA("BasePart") then
-                        x.Velocity, x.RotVelocity = Vector3.new(), Vector3.new()
-                    end
+            table.foreach(Character:GetChildren(), function(_, x)
+                if x:IsA("BasePart") then
+                    x.Velocity, x.RotVelocity = Vector3.new(), Vector3.new()
                 end
-            )
+            end)
             task.wait()
         until (RootPart.Position - getgenv().OldPos.p).Magnitude < 25
         workspace.FallenPartsDestroyHeight = getgenv().FPDH
     end
 end
 
-if Targets[1] then
-    for _, x in next, Targets do
-        GetPlayer(x)
-    end
-else
-    return
-end
+if Targets[1] then for _,x in next, Targets do GetPlayer(x) end else return end
 
-function Functions:flingAll()
-    if AllBool then
-        for _, x in next, Players:GetPlayers() do
-            SkidFling(x)
-        end
+ function Functions:flingAll()
+if AllBool then
+    for _,x in next, Players:GetPlayers() do
+        SkidFling(x)
     end
+end
 end
 
 function Functions:Fling()
-    for _, x in next, Targets do
+    for _,x in next, Targets do
         if GetPlayer(x) and GetPlayer(x) ~= Player then
             if GetPlayer(x).Name ~= "GGH52Lan" then
                 local TPlayer = GetPlayer(x)
@@ -266,10 +210,11 @@ function Functions:Fling()
     end
 end
 
-function Functions:GetTarget(text)
+ function Functions:GetTarget(text)
     if text then
-        Targets[1] = tostring(text)
+    Targets[1] = tostring(text)
     end
+ -- return Targets
 end
 
 return Functions
